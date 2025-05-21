7c9214dfe633632d50785122a2ccf1ff529131db
DartViet <darter_vn@engineer.com>
2025-05-21 08:21:21 -0700

    fix: test

--- lib/pages/user_info/user_info.dart
+++ lib/pages/user_info/user_info.dart
@@ -33,136 +33,132 @@ class UserInfoState extends State<UserInfo> {
     return Scaffold(
       appBar: FAppbar(),
       drawer: FDrawer().getDrawer(context),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
-          child: SizedBox(
-            width: 500,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 16,
-
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "User Information",
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    Expanded(child: Container()),
-                    IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: () {
-                        // Handle edit action
-                      },
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.save),
-                      onPressed: () {
-                        // Handle save action
-                      },
-                    ),
-                  ],
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Full Name',
-                    border: OutlineInputBorder(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 16,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "User Information",
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'CCCD',
-                    border: OutlineInputBorder(),
+                  Expanded(child: Container()),
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      // Handle edit action
+                    },
                   ),
-                ),
-                Row(
-                  children: [
-                    Text('Ngày Cấp Thẻ: '),
-                    TextButton(
-                      onPressed: selectDate,
-                      child: Text(
-                        selectedDate == null
-                            ? 'Select date'
-                            : '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}',
-                      ),
-                    ),
-                  ],
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'CMND',
-                    border: OutlineInputBorder(),
+                  IconButton(
+                    icon: const Icon(Icons.save),
+                    onPressed: () {
+                      // Handle save action
+                    },
                   ),
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Full Name',
+                  border: OutlineInputBorder(),
                 ),
-
-                Row(
-                  children: [
-                    Text('Date of Birth: '),
-                    TextButton(
-                      onPressed: selectDate,
-                      child: Text(
-                        selectedDate == null
-                            ? 'Select date'
-                            : '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}',
-                      ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'CCCD',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Row(
+                children: [
+                  Text('Ngày Cấp Thẻ: '),
+                  TextButton(
+                    onPressed: selectDate,
+                    child: Text(
+                      selectedDate == null
+                          ? 'Select date'
+                          : '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}',
                     ),
-                  ],
+                  ),
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'CMND',
+                  border: OutlineInputBorder(),
                 ),
+              ),
 
-                Row(
-                  children: [
-                    Text("Sex: "),
-                    DropdownButton<String>(
-                      value: "nam",
-                      items: [
-                        DropdownMenuItem(value: "nam", child: Text("Nam")),
-                        DropdownMenuItem(value: "nu", child: Text("Nữ")),
+              Row(
+                children: [
+                  Text('Date of Birth: '),
+                  TextButton(
+                    onPressed: selectDate,
+                    child: Text(
+                      selectedDate == null
+                          ? 'Select date'
+                          : '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}',
+                    ),
+                  ),
+                ],
+              ),
+
+              Row(
+                children: [
+                  Text("Sex: "),
+                  DropdownButton<String>(
+                    value: "nam",
+                    items: [
+                      DropdownMenuItem(value: "nam", child: Text("Nam")),
+                      DropdownMenuItem(value: "nu", child: Text("Nữ")),
+                    ],
+                    onChanged: (value) {
+                      // Handle the change
+                    },
+                  ),
+                ],
+              ),
+
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Address',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    width: 200,
+                    child: Row(
+                      spacing: 32,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.qr_code_scanner),
+                        Text("Scan QR Code"),
                       ],
-                      onChanged: (value) {
-                        // Handle the change
-                      },
-                    ),
-                  ],
-                ),
-
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Address',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Phone Number',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Container(
-                      width: 200,
-                      child: Row(
-                        spacing: 32,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.qr_code_scanner),
-                          Text("Scan QR Code"),
-                        ],
-                      ),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
